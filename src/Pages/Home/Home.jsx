4a840b877f7cb27872553a3b46519994855fdb9c
fadhelbenosmtane <lf_benostmane@esi.dz>
2023-03-06 02:25:47 +0100

    commit 2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,22 +4,6 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 function Home(props) {
-    const [name,setName] = useState(true)
-    function updateNum(){
-        setName(!name)
-    }
-
-    const [name,setName]=useState(true)
-    function updateName(){
-        setName(!name)
-    }
-    function print(){
-    if(name) {
-        return 'fadhel'
-    }
-    else{
-        return 'mkhkhkhkhk'
-    }}
     return (
         <div>
             <NavigationBar title="Fadhel"></NavigationBar>
