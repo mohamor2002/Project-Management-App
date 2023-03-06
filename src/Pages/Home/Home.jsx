d1ceb62ea89cb20d0c8464182e185970d6c25003
fadhelbenosmtane <lf_benostmane@esi.dz>
2023-03-06 01:35:14 +0100

    commit 3

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { useState } from 'react';
 function Home(props) {
+    const [count,setCount]=useState(0)
+    function updateNum(){
+        setCount(count+1)
+
+
+    }
     return (
         <div>
-            howm
+            <button onClick={updateNum}>3abez</button>
+            <div>{count}</div>
         </div>
     );
 }
