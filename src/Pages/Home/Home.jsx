5ad422e4d627c77ae99419a7656672a5bb1baf7a
fadhelbenosmtane <lf_benostmane@esi.dz>
2023-03-06 23:52:38 +0100

    commit1

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,16 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 function Home(props) {
+    const i=1
+    const [name,setName] = useState(true)
+    function updateNum(){
+        setName(!name)
+    }
     return (
         <div>
             <NavigationBar title="Fadhel"></NavigationBar>
             <button onClick={updateNum}>{name?'moh':'fadhel'}</button>
+            <button onClick={} ></button>
         </div>
     );
 }
